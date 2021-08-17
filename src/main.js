ef3168163835972458b809f3b6d46b2023fb1191
fabienkerinec <fabien.kerinec@gmail.com>
2021-08-17 14:24:53 +0200

    initial change

--- src/main.js
+++ src/main.js
@@ -12,6 +12,8 @@ let loader = lottie.loadAnimation({
 let max = 70
 let index = 0
 let percent = 0
+let current = 1
+let next = 2
 let loaderInterval = setInterval(() => {
   index++
   percent = (index * max) / 100
@@ -20,5 +22,77 @@ let loaderInterval = setInterval(() => {
   if (index >= 100) {
     clearInterval(loaderInterval)
     loaderElement.classList.add('finished')
+    // setTimeout(() => {
+    //   document.querySelector("section[data-id='"+current+"']").classList.add("displayup")
+    //   setTimeout(() => {
+    //     document.querySelector("section[data-id='"+current+"']").classList.add("display")
+    //   }, 2000);
+    // }, 200);
+
+    setTimeout(() => {
+      document.querySelector("section[data-id='"+current+"']").classList.add("displaydown")
+      setTimeout(() => {
+        document.querySelector("section[data-id='"+current+"']").classList.add("display")
+      }, 2000);
+    }, 200);
+    // nextItem(current, next)
   }
 }, 10)
+
+function nextItem (current, next){
+  console.log(current, next);
+  setTimeout(() => {
+    document.querySelector("section[data-id='"+current+"']").classList.add("mask", "maskup")
+    setTimeout(() => {
+      document.querySelector("section[data-id='"+next+"']").classList.add("displayup")
+      setTimeout(() => {
+      
+        document.querySelector("section[data-id='"+next+"']").classList.add("display")
+      }, 100);
+      document.querySelector("section[data-id='"+current+"']").classList.remove("mask", "maskup", "maskdown", "display", "displayup", "displaydown")
+      current = next;
+      triggerNavigate = false
+    }, 1500);
+  }, 0);
+}
+function prevItem(current, next){
+  setTimeout(() => {
+    document.querySelector("section[data-id='"+current+"']").classList.add("mask", "maskdown")
+    setTimeout(() => {
+      document.querySelector("section[data-id='"+next+"']").classList.add("displaydown")
+      setTimeout(() => {
+        document.querySelector("section[data-id='"+next+"']").classList.add("display")
+      }, 100);
+      document.querySelector("section[data-id='"+current+"']").classList.remove("mask", "maskup", "maskdown", "display", "displayup", "displaydown")
+      current = next;
+      triggerNavigate = false
+    }, 1500);
+  }, 0);
+}
+const navigateList = document.querySelectorAll(".navigate ul li")
+let triggerNavigate = false
+navigateList.forEach(item => {
+  item.addEventListener("click", function(){
+    if(!triggerNavigate){
+      triggerNavigate = true
+      choose(this.dataset.id) 
+    }
+  })
+});
+
+
+function choose(number){
+  console.log("yooo");
+  console.log(current, parseInt(number));
+  if(parseInt(current) > parseInt(number)){
+    console.log("yooo1");
+    prevItem(parseInt(current), parseInt(number))
+    current = parseInt(number)
+  }else if (parseInt(current) < parseInt(number)){
+    console.log("yooo2");
+    nextItem(parseInt(current), parseInt(number))
+    current = parseInt(number)
+  }else{
+    triggerNavigate = false
+  }
+}
